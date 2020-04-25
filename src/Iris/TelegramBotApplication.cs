8422c93bfcd5e5c5a330e516af1c5528e79006fa
TheMulti0 <multi@codeprecise.com>
2020-04-25 18:17:32 +0300

    Use real application

--- src/Iris/TelegramBotApplication.cs
+++ src/Iris/TelegramBotApplication.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.IO;
+using System.Text.Json;
 using System.Threading.Tasks;
+using Iris.Config;
+using Iris.Logging;
+using Microsoft.Extensions.Logging;
+using Updates.Watcher;
 
 namespace Iris
 {
@@ -7,32 +13,28 @@ namespace Iris
     {
         private static async Task Main(string[] args)
         {
+#if DEBUG
+            const string rootDirectory = "../../..";
+#else
+            const string rootDirectory = "config";
+#endif
             
-            Console.Write("HELLO!!!!!!!! \n \n \n ");
-            Console.ReadLine();
+            var config = await JsonSerializer
+                .DeserializeAsync<ApplicationConfig>(
+                    new FileStream($"{rootDirectory}/appsettings.json", FileMode.Open));
+
+            ILoggerFactory factory = LoggerFactory
+                .Create(builder => builder
+                    .AddConsole()
+                    .AddFile(options => options.LogDirectory = "logs"));
+        
+            var telegramBot = new Bot.Bot(
+                config,
+                factory,
+                $"{rootDirectory}/{config.TelegramBotConfig.ChatsFile}",
+                new JsonUpdateValidator($"{rootDirectory}/{config.TelegramBotConfig.SavedUpdatesFile}"));
+
             await Task.Delay(-1);
-// #if DEBUG
-//             const string rootDirectory = "../../..";
-// #else
-//             const string rootDirectory = "config";
-// #endif
-//             
-//             var config = await JsonSerializer
-//                 .DeserializeAsync<ApplicationConfig>(
-//                     new FileStream($"{rootDirectory}/appsettings.json", FileMode.Open));
-//
-//             ILoggerFactory factory = LoggerFactory
-//                 .Create(builder => builder
-//                     .AddConsole()
-//                     .AddFile(options => options.LogDirectory = "logs"));
-//         
-//             var telegramBot = new Bot.Bot(
-//                 config,
-//                 factory,
-//                 $"{rootDirectory}/{config.TelegramBotConfig.ChatsFile}",
-//                 new JsonUpdateValidator($"{rootDirectory}/{config.TelegramBotConfig.SavedUpdatesFile}"));
-//
-//             await Task.Delay(-1);
         }
     }
 }
