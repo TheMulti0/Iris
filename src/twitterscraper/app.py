af11cd0e5f97adbca11949993738ebbd0f1b62d4
TheMulti0 <multi@codeprecise.com>
2020-04-26 19:00:47 +0300

    twitterscraper

---
+++ src/twitterscraper/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+from twitter_scraper import get_tweets
+
+app = Flask(__name__)
+
+
+@app.route('/tweets', methods=['GET'])
+def get_twitter_tweets():
+    name = request.args['name']
+    page_count = int(request.args['page_count'])
+
+    posts = list(
+        get_tweets(
+            name,
+            pages=page_count))
+
+    return jsonify(posts)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
