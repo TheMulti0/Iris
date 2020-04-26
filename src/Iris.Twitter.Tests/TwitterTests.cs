3a0d29b88dadbacbb92fa08c3e06943f39263641
TheMulti0 <multi@codeprecise.com>
2020-04-26 18:18:47 +0300

    Updates.Twitter.Tests -> Iris.Twitter.Tests

--- src/Iris.Twitter.Tests/TwitterTests.cs
+++ src/Iris.Twitter.Tests/TwitterTests.cs
@@ -1,11 +1,10 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Iris.Api;
-using Iris.Twitter;
 using Microsoft.Extensions.Logging.Abstractions;
 using Xunit;
 
-namespace Updates.Twitter.Tests
+namespace Iris.Twitter.Tests
 {
     public class TwitterTests
     {
@@ -14,7 +13,7 @@ namespace Updates.Twitter.Tests
         {
             var config = JsonExtensions.Read<TwitterConfig>("../../../appsettings.json");
             
-            var twitter = new Iris.Twitter.Twitter(NullLogger<Iris.Twitter.Twitter>.Instance, config);
+            var twitter = new Twitter(NullLogger<Twitter>.Instance, config);
             IEnumerable<Update> updates = await twitter.GetUpdates("@realDonaldTrump");
             foreach (Update update in updates)
             {
