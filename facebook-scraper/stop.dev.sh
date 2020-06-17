b86db2865eafe1a7e33a686bb75e3ef88bdbfa08
TheMulti0 <multi@codeprecise.com>
2020-06-17 13:58:02 +0300

    Replaced dev scripts with docker-compose.dev

--- facebook-scraper/stop.dev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Stopping facebook-scraper"
-
-docker container rm -f facebook-scraper
-docker system prune -f
--- twitter-scraper/stop.dev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Stopping twitter-scraper"
-
-docker container rm -f twitter-scraper
-docker system prune -f
