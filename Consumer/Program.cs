0792980224544554c98bef5481fa6ef1c5b7aaa0
TheMulti0 <multi@codeprecise.com>
2020-06-27 22:28:42 +0300

    Producer - Consumer communication using Kafka

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using Confluent.Kafka;
 
 namespace Consumer
 {
@@ -6,7 +8,41 @@ namespace Consumer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var conf = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using var c = new ConsumerBuilder<Ignore, string>(conf).Build();
+            c.Subscribe("test-topic");
+
+            // Because Consume is a blocking call, we want to capture Ctrl+C and use a cancellation token to get out of our while loop and close the consumer gracefully.
+            var cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) => {
+                e.Cancel = true;
+                cts.Cancel();
+            };
+
+            try
+            {
+                while (true)
+                {
+                    // Consume a message from the test topic. Pass in a cancellation token so we can break out of our loop when Ctrl+C is pressed
+                    var cr = c.Consume(cts.Token);
+                    Console.WriteLine($"Consumed message '{cr.Value}' from topic {cr.Topic}, partition {cr.Partition}, offset {cr.Offset}");
+
+                    // Do something interesting with the message you consumed
+                }
+            }
+            catch (OperationCanceledException)
+            {
+            }
+            finally
+            {
+                c.Close();
+            }
         }
     }
 }
