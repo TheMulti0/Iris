8ae1f20955873c15eb5f9a12a5684a99273dd734
TheMulti0 <multi@codeprecise.com>
2020-06-28 08:40:28 +0300

    Consume and Produce JSON objects

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -1,9 +1,23 @@
 ﻿using System;
+using System.Text.Json;
 using System.Threading;
 using Confluent.Kafka;
 
 namespace Consumer
 {
+    class Update 
+    {
+        public string Content { get; set; }
+    }
+
+    class UpdateDeserializer : IDeserializer<Update>
+    {
+        public Update Deserialize(ReadOnlySpan<byte> data, bool isNull, SerializationContext context)
+        {
+            return JsonSerializer.Deserialize<Update>(data);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -15,7 +29,7 @@ namespace Consumer
                 AutoOffsetReset = AutoOffsetReset.Earliest
             };
 
-            using var c = new ConsumerBuilder<Ignore, string>(conf).Build();
+            using var c = new ConsumerBuilder<Ignore, Update>(conf).SetValueDeserializer(new UpdateDeserializer()).Build();
             c.Subscribe("test-topic");
 
             // Because Consume is a blocking call, we want to capture Ctrl+C and use a cancellation token to get out of our while loop and close the consumer gracefully.
@@ -31,7 +45,7 @@ namespace Consumer
                 {
                     // Consume a message from the test topic. Pass in a cancellation token so we can break out of our loop when Ctrl+C is pressed
                     var cr = c.Consume(cts.Token);
-                    Console.WriteLine($"Consumed message '{cr.Value}' from topic {cr.Topic}, partition {cr.Partition}, offset {cr.Offset}");
+                    Console.WriteLine($"Consumed message '{cr.Message.Value.Content}' from topic {cr.Topic}, partition {cr.Partition}, offset {cr.Offset}");
 
                     // Do something interesting with the message you consumed
                 }
--- Producer/Program.cs
+++ Producer/Program.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Confluent.Kafka;
 
 namespace Producer
 {
+    class Update 
+    {
+        public string Content { get; set; }
+    }
+
+    class UpdateSerializer : ISerializer<Update>
+    {
+        public byte[] Serialize(Update data, SerializationContext context)
+        {
+            return JsonSerializer.SerializeToUtf8Bytes(data);
+        }
+    }
+
     class Program
     {
         private static async Task Main(string[] args)
@@ -15,15 +29,18 @@ namespace Producer
             };
 
             // Create a producer that can be used to send messages to kafka that have no key and a value of type string 
-            using var p = new ProducerBuilder<Null, string>(config).Build();
+            using var p = new ProducerBuilder<Null, Update>(config).SetValueSerializer(new UpdateSerializer()).Build();
 
             var i = 0;
             while (true)
             {
                 // Construct the message to send (generic type must match what was used above when creating the producer)
-                var message = new Message<Null, string>
+                var message = new Message<Null, Update>
                 {
-                    Value = $"Message #{++i}"
+                    Value = new Update 
+                    {
+                        Content = $"Message #{++i}"
+                    }
                 };
 
                 // Send the message to our test topic in Kafka                
