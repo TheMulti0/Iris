2c7c31690c03ca358a67477c2af7952e3ebc237c
TheMulti0 <multi@codeprecise.com>
2020-06-28 17:02:05 +0300

    Updated Update.cs

--- Consumer/Update.cs
+++ Consumer/Update.cs
@@ -1,7 +1,17 @@
+using System;
+using System.Text.Json.Serialization;
+
 namespace Consumer
 {
     public class Update
     {
+        [JsonPropertyName("content")]
         public string Content { get; set; }
+
+        [JsonPropertyName("creation_date")]
+        public DateTime CreationDate { get; set; }
+        
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
     }
 }
