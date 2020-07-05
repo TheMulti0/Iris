a5c9498310c611086f9baf96cab86eada4a4ea07
TheMulti0 <multi@codeprecise.com>
2020-07-05 10:21:51 +0300

    Added Hosting for TelegramConsumer

--- TelegramConsumer/TelegramSender.cs
+++ TelegramConsumer/TelegramSender.cs
@@ -1,15 +1,34 @@
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Telegram.Bot;
 
 namespace TelegramConsumer
 {
+    public class TelegramConfig
+    {
+        public string AccessToken { get; set; }
+    }
+
     public class TelegramSender : ISender
     {
+        private readonly TelegramBotClient _client;
         private readonly ILogger<TelegramSender> _logger;
 
-        public TelegramSender(ILogger<TelegramSender> logger)
+        public TelegramSender(
+            TelegramConfig config,
+            ILogger<TelegramSender> logger)
         {
+            _client = new TelegramBotClient(config.AccessToken);
             _logger = logger;
+
+            var identity = _client.GetMeAsync().Result;
+
+            _logger.LogInformation(
+                "Registered as {} {} (Username = {}, Id = {})",
+                identity.FirstName,
+                identity.LastName,
+                identity.Username,
+                identity.Id);
         }
 
         public Task SendAsync(Update update)
