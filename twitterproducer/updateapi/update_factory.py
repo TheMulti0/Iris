7a330af3fb4dfa6a7f71cf27ac00cc24fafa5648
TheMulti0 <multi@codeprecise.com>
2020-07-22 10:16:23 +0300

    Regex to remove pic.twitter link

--- twitterproducer/updateapi/update_factory.py
+++ twitterproducer/updateapi/update_factory.py
@@ -1,3 +1,5 @@
+import re
+
 from updatesproducer.updateapi.update import Update
 from twitterproducer.updateapi.media_factory import MediaFactory
 from twitterproducer.tweets.tweet import Tweet
@@ -7,7 +9,8 @@ class UpdateFactory:
     @staticmethod
     def to_update(tweet: Tweet):
         return Update(
-            content=tweet.text,
+            # Replace pic.twitter.com/232dssad links with nothing
+            content=re.sub('pic.twitter.com/\\S+', '', tweet.text),
             author_id=tweet.username,
             creation_date=tweet.time,
             url=tweet.tweetUrl,
