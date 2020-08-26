6da1823e6d8b369402f21615ea08f911ee7269bb
TheMulti0 <multi@codeprecise.com>
2020-08-26 19:27:27 +0300

    Filter tweet replies

--- twitterproducer/tweets/tweets_provider.py
+++ twitterproducer/tweets/tweets_provider.py
@@ -13,4 +13,7 @@ class TweetsProvider(ITweetsProvider):
         self.__logger = logger
 
     def get_tweets(self, user_id):
-        return self.__api.user_timeline(screen_name=user_id, tweet_mode="extended")
+        return filter(
+            # Tweet is not a reply tweet
+            lambda t: t.in_reply_to_status_id_str is None,
+            self.__api.user_timeline(screen_name=user_id, tweet_mode="extended"))
