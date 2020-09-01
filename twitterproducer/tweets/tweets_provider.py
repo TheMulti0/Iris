80c2d9fd63340c8e048a41d8674535bb9a07b3f3
TheMulti0 <multi@codeprecise.com>
2020-09-01 19:09:13 +0300

    Take tweet author replies too

--- twitterproducer/tweets/tweets_provider.py
+++ twitterproducer/tweets/tweets_provider.py
@@ -13,7 +13,14 @@ class TweetsProvider(ITweetsProvider):
         self.__logger = logger
 
     def get_tweets(self, user_id):
+        def is_tweet_relevant(t):
+            if t.in_reply_to_status_id_str is not None:
+                if t.in_reply_to_screen_name == user_id:
+                    return True
+                return False
+
+            return True
+
         return filter(
-            # Tweet is not a reply tweet
-            lambda t: t.in_reply_to_status_id_str is None,
+            is_tweet_relevant,
             self.__api.user_timeline(screen_name=user_id, tweet_mode="extended"))
