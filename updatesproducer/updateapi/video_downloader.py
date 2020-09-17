8b15a6d6bc1a25bf01791bf0a553006a308afded
TheMulti0 <multi@codeprecise.com>
2020-09-17 12:02:18 +0300

    Construct video in VideoDownloader

--- updatesproducer/updateapi/video_downloader.py
+++ updatesproducer/updateapi/video_downloader.py
@@ -15,12 +15,22 @@ class VideoDownloader:
         # If a video is found
         if video is not None:
 
+            thumbnail = video.thumbnails.get('high')
+            if thumbnail is None:
+                thumbnail = video.thumbnails.get('default')
+
             # Remove all old lowres that were found
             for lowres_video in lowres_videos:
                 update.media.remove(lowres_video)
 
             # Add new downloaded video
-            update.media.append(Video(video))
+            update.media.append(Video(
+                url=video['url'],
+                thumbnail_url=thumbnail['url'],
+                duration_seconds=video['duration'],
+                width=video['width'],
+                height=video['height']
+            ))
 
     def _try_download_video(self, url):
         ydl_opts = {
@@ -30,7 +40,7 @@ class VideoDownloader:
 
         try:
             with YoutubeDL(ydl_opts) as ydl:
-                return ydl.extract_info(url, download=False)['url']
+                return ydl.extract_info(url, download=False)
 
         except BaseException as ex:
             self.__logger.error("Error extracting video with youtube-dl, skipping video check: %r", ex)
