07510ec4ac9ed6f48a7ed5cb82e1278abc42d50a
TheMulti0 <multi@codeprecise.com>
2020-10-04 22:50:10 +0300

    Added DataLayer

---
+++ DataLayer/IUpdatesRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UpdatesConsumer;
+
+namespace DataLayer
+{
+    public interface IUpdatesRepository
+    {
+        Task<IEnumerable<Update>> GetAsync();
+        
+        Task AddAsync(Update update);
+        
+        Task RemoveAsync(Update update);
+    }
+}
---
+++ DataLayer/MongoUpdatesRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using UpdatesConsumer;
+
+namespace DataLayer
+{
+    public class MongoUpdatesRepository : IUpdatesRepository
+    {
+        private readonly IMongoCollection<Update> _updates;
+
+        public MongoUpdatesRepository(string connectionString, string databaseName, string collection)
+        {
+            var client = new MongoClient(connectionString);
+            IMongoDatabase database = client.GetDatabase(databaseName);
+
+            _updates = database.GetCollection<Update>(collection);
+        }
+
+        public async Task<IEnumerable<Update>> GetAsync()
+        {
+            IAsyncCursor<Update> cursor = await _updates.FindAsync(_ => true);
+            
+            return await cursor.ToListAsync();
+        }
+
+        public Task AddAsync(Update update) 
+            => _updates.InsertOneAsync(update);
+
+        public Task RemoveAsync(Update update) 
+            => _updates.DeleteOneAsync(i => i.Id == update.Id);
+    }
+}
