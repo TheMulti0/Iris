b3b94319a8ae72e6bd3143026b2e3b5b02d8554c
TheMulti0 <multi@codeprecise.com>
2020-10-27 08:29:16 +0200

    Support exceptions in CustomConsoleLogger

--- Extensions/CustomConsoleLogger/CustomConsoleLogger.cs
+++ Extensions/CustomConsoleLogger/CustomConsoleLogger.cs
@@ -23,10 +23,17 @@ namespace Extensions
 
             var formattedDate = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss zzz");
             var formattedLevel = logLevel.ToString();
-            string formattedMessage = formatter(state, exception);
+            string formattedMessage = Format(state, exception);
 
             Console.WriteLine(
                 $"[{formattedDate}] [{formattedLevel}] [{_name}] {formattedMessage}");
+        }
+
+        private static string Format<TState>(TState state, Exception exception)
+        {
+            return exception != null 
+                ? $"{state}\n{exception}" 
+                : state.ToString();
         }
 
         public bool IsEnabled(LogLevel logLevel) => true;
