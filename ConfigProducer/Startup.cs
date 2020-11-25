8129bbd2f039e2b3b883df5ac18a2f6d9b56943f
TheMulti0 <multi@codeprecise.com>
2020-11-25 09:27:01 +0200

    Top down startup statements

--- ConfigProducer/Startup.cs
+++ ConfigProducer/Startup.cs
@@ -1,65 +1,58 @@
 using System;
 using System.IO;
-using System.Threading.Tasks;
+using ConfigProducer;
 using Extensions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace ConfigProducer
+static void ConfigureConfiguration(IConfigurationBuilder builder)
 {
-    public class Startup
-    {
-        public static Task Main()
-        {
-            return new HostBuilder()
-                .ConfigureAppConfiguration(ConfigureConfiguration)
-                .ConfigureLogging(ConfigureLogging)
-                .ConfigureServices(ConfigureServices)
-                .RunConsoleAsync();
-        }
+    string environmentName =
+        Environment.GetEnvironmentVariable("ENVIRONMENT");
 
-        private static void ConfigureConfiguration(IConfigurationBuilder builder)
-        {
-            string environmentName =
-                Environment.GetEnvironmentVariable("ENVIRONMENT");
+    const string fileName = "appsettings";
+    const string fileType = "json";
 
-            const string fileName = "appsettings";
-            const string fileType = "json";
-
-            builder
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile($"{fileName}.{fileType}", false)
-                .AddJsonFile($"{fileName}.{environmentName}.{fileType}", true); // Overrides default appsettings.json
-        }
-
-        private static void ConfigureLogging(HostBuilderContext context, ILoggingBuilder builder)
-        {
-            builder
-                .AddConfiguration(context.Configuration.GetSection("Logging"))
-                .AddCustomConsole();
-        }
-        
-        private static void ConfigureServices(IServiceCollection services)
-        {
-            services.AddSingleton(GetConfigsReaderConfig)
-                .AddSingleton(GetKafkaConfig)
-                .AddSingleton<ConfigReader>()
-                .AddProducer<string, string>()
-                .AddHostedService<ConfigProducer>();
-        }
-        
-        private static ConfigReaderConfig GetConfigsReaderConfig(IServiceProvider provider)
-        {
-            return provider.GetService<IConfiguration>().GetSection("ConfigsReader")
-                .Get<ConfigReaderConfig>();
-        }
-    
-        private static BaseKafkaConfig GetKafkaConfig(IServiceProvider provider)
-        {
-            return provider.GetService<IConfiguration>().GetSection("Kafka")
-                .Get<BaseKafkaConfig>();
-        }
-    }
+    builder
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile($"{fileName}.{fileType}", false)
+        .AddJsonFile($"{fileName}.{environmentName}.{fileType}", true); // Overrides default appsettings.json
 }
+
+static void ConfigureLogging(HostBuilderContext context, ILoggingBuilder builder)
+{
+    builder
+        .AddConfiguration(context.Configuration.GetSection("Logging"))
+        .AddCustomConsole();
+}
+
+static void ConfigureServices(IServiceCollection services)
+{
+    services.AddSingleton(GetConfigsReaderConfig)
+        .AddSingleton(GetKafkaConfig)
+        .AddSingleton<ConfigReader>()
+        .AddProducer<string, string>()
+        .AddHostedService<ConfigProducer.ConfigProducer>();
+}
+
+static ConfigReaderConfig GetConfigsReaderConfig(IServiceProvider provider)
+{
+    return provider.GetService<IConfiguration>()
+        ?.GetSection("ConfigsReader")
+        .Get<ConfigReaderConfig>();
+}
+
+static BaseKafkaConfig GetKafkaConfig(IServiceProvider provider)
+{
+    return provider.GetService<IConfiguration>()
+        ?.GetSection("Kafka")
+        .Get<BaseKafkaConfig>();
+}
+
+await new HostBuilder()
+    .ConfigureAppConfiguration(ConfigureConfiguration)
+    .ConfigureLogging(ConfigureLogging)
+    .ConfigureServices(ConfigureServices)
+    .RunConsoleAsync();
--- TelegramBot/Startup.cs
+++ TelegramBot/Startup.cs
@@ -1,89 +1,80 @@
 using System;
 using System.IO;
 using System.Text.Json;
-using System.Threading.Tasks;
 using Extensions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Sentry;
-using Sentry.Extensions.Logging;
-using Sentry.Protocol;
+using TelegramBot;
 using UpdatesConsumer;
 
-namespace TelegramBot
+static void ConfigureConfiguration(IConfigurationBuilder builder)
 {
-    public static class Startup
+    string environmentName =
+        Environment.GetEnvironmentVariable("ENVIRONMENT");
+
+    const string fileName = "appsettings";
+    const string fileType = "json";
+
+    builder
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile($"{fileName}.{fileType}", false)
+        .AddJsonFile($"{fileName}.{environmentName}.{fileType}", true); // Overrides default appsettings.json
+}
+
+static void ConfigureLogging(HostBuilderContext context, ILoggingBuilder builder)
+{
+    builder
+        .AddConfiguration(context.Configuration)
+        .AddCustomConsole();
+
+    if (context.Configuration.GetSection("Sentry")
+        .Exists())
     {
-        public static Task Main() => new HostBuilder()
-            .ConfigureAppConfiguration(ConfigureConfiguration)
-            .ConfigureLogging(ConfigureLogging)
-            .ConfigureServices(ConfigureServices)
-            .RunConsoleAsync();
-
-        private static void ConfigureConfiguration(IConfigurationBuilder builder)
-        {
-            string environmentName =
-                Environment.GetEnvironmentVariable("ENVIRONMENT");
-
-            const string fileName = "appsettings";
-            const string fileType = "json";
-
-            builder
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile($"{fileName}.{fileType}", false)
-                .AddJsonFile($"{fileName}.{environmentName}.{fileType}", true); // Overrides default appsettings.json
-        }
-
-        private static void ConfigureLogging(HostBuilderContext context, ILoggingBuilder builder)
-        {
-            builder
-                .AddConfiguration(context.Configuration)
-                .AddCustomConsole();
-
-            if (context.Configuration.GetSection("Sentry")
-                .Exists())
-            {
-                builder.AddSentry();
-            }
-        }
-
-        private static void ConfigureServices(HostBuilderContext hostContext, IServiceCollection services)
-        {
-            IConfiguration rootConfig = hostContext.Configuration;
-
-            var updatesConsumerConfig = rootConfig
-                .GetSection("UpdatesConsumer")
-                ?
-                .Get<ConsumerConfig>();
-
-            var configConsumerConfig = rootConfig
-                .GetSection("ConfigConsumer")
-                ?.Get<ConsumerConfig>();
-
-            var defaultTelegramConfig = rootConfig
-                .GetSection("Telegram")
-                ?.Get<TelegramConfig>();
-
-            services
-                .AddConsumer<string, Update>(
-                    updatesConsumerConfig,
-                    new JsonSerializerOptions
-                    {
-                        Converters =
-                        {
-                            new MediaJsonSerializer()
-                        }
-                    })
-                .AddConsumer<string, string>(configConsumerConfig)
-                .AddSingleton(defaultTelegramConfig)
-                .AddSingleton<IConfigProvider, ConfigProvider>()
-                .AddSingleton<ITelegramBotClientProvider, TelegramBotClientProvider>()
-                .AddSingleton<MessageBuilder>()
-                .AddSingleton<IUpdateConsumer, TelegramBot>()
-                .AddHostedService<UpdatesConsumerService>()
-                .BuildServiceProvider();
-        }
+        builder.AddSentry();
     }
 }
+
+static void ConfigureServices(HostBuilderContext hostContext, IServiceCollection services)
+{
+    IConfiguration rootConfig = hostContext.Configuration;
+
+    var updatesConsumerConfig = rootConfig
+        .GetSection("UpdatesConsumer")
+        ?
+        .Get<ConsumerConfig>();
+
+    var configConsumerConfig = rootConfig
+        .GetSection("ConfigConsumer")
+        ?.Get<ConsumerConfig>();
+
+    var defaultTelegramConfig = rootConfig
+        .GetSection("Telegram")
+        ?.Get<TelegramConfig>();
+
+    services
+        .AddConsumer<string, Update>(
+            updatesConsumerConfig,
+            new JsonSerializerOptions
+            {
+                Converters =
+                {
+                    new MediaJsonSerializer()
+                }
+            })
+        .AddConsumer<string, string>(configConsumerConfig)
+        .AddSingleton(defaultTelegramConfig)
+        .AddSingleton<IConfigProvider, ConfigProvider>()
+        .AddSingleton<ITelegramBotClientProvider, TelegramBotClientProvider>()
+        .AddSingleton<MessageBuilder>()
+        .AddSingleton<IUpdateConsumer, TelegramBot.TelegramBot>()
+        .AddHostedService<UpdatesConsumerService>()
+        .BuildServiceProvider();
+}
+    
+await new HostBuilder()
+    .ConfigureAppConfiguration(ConfigureConfiguration)
+    .ConfigureLogging(ConfigureLogging)
+    .ConfigureServices(ConfigureServices)
+    .RunConsoleAsync();
