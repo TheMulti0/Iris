64152ff6edcf276f2f523ee20cb5ba8e232f7910
TheMulti0 <multi@codeprecise.com>
2020-12-28 21:11:35 +0200

    ChatPollRequests are already going to db

--- PollRulesManager/PollRequests/ChatPollRequestsConsumer.cs
+++ PollRulesManager/PollRequests/ChatPollRequestsConsumer.cs
@@ -10,16 +10,13 @@ namespace PollRulesManager
 {
     public class ChatPollRequestsConsumer : IConsumer<ChatPollRequest>
     {
-        private readonly ISavedUsersRepository _repository;
         private readonly IProducer<ChatPollRequest> _producer;
         private readonly ILogger<ChatPollRequestsConsumer> _logger;
 
         public ChatPollRequestsConsumer(
-            ISavedUsersRepository repository,
             IProducer<PollRequest> producer,
             ILogger<ChatPollRequestsConsumer> logger)
         {
-            _repository = repository;
             _producer = producer;
             _logger = logger;
         }
@@ -27,20 +24,7 @@ namespace PollRulesManager
         public async Task ConsumeAsync(ChatPollRequest request, CancellationToken token)
         {
             _logger.LogInformation("Received chat poll request {}", request);
-
-            if (request.Request == Request.StartPoll)
-            {
-                await _repository.AddOrUpdateAsync(request.PollRule.User, new UserChatInfo
-                {
-                    ChatId = request.ChatId,
-                    Interval = (TimeSpan) request.PollRule.Interval
-                });
-            }
-            else
-            {
-                await _repository.RemoveAsync(request.PollRule.User, request.ChatId);
-            }
-
+            
             _producer.Send(request);
         }
     }
