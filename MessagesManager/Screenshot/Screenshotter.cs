af5f7a74cca2d45b2a2935846e6be64747e86915
TheMulti0 <multi@codeprecise.com>
2021-02-19 09:20:50 +0200

    Catch exception

--- MessagesManager/Screenshot/Screenshotter.cs
+++ MessagesManager/Screenshot/Screenshotter.cs
@@ -1,27 +1,40 @@
-﻿using System.Drawing.Imaging;
+﻿using System;
+using System.Drawing.Imaging;
 using System.Threading.Tasks;
 using Common;
+using Microsoft.Extensions.Logging;
 
 namespace MessagesManager
 {
     internal class Screenshotter
     {
         private readonly TwitterScreenshotter _twitter;
+        private readonly ILogger<Screenshotter> _logger;
 
-        public Screenshotter(TwitterScreenshotter twitter)
+        public Screenshotter(
+            TwitterScreenshotter twitter,
+            ILogger<Screenshotter> logger)
         {
             _twitter = twitter;
+            _logger = logger;
         }
 
         public async Task<byte[]> ScreenshotAsync(Update update)
         {
-            switch (update.Author.Platform)
+            try
             {
-                case Platform.Twitter:
-                    return _twitter.Screenshot(update.Url).ToByteArray(ImageFormat.Png);
-                    
-                default:
-                    return null;
+                return update.Author.Platform switch 
+                {
+                    Platform.Twitter 
+                        => _twitter.Screenshot(update.Url).ToByteArray(ImageFormat.Png),
+                    _ 
+                        => null
+                };
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Failed to screenshot {}", update);
+                return null;
             }
         }
     }
