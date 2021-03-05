6b6823252cca7d347af16c19a8cbf8149506c60a
TheMulti0 <multi@codeprecise.com>
2021-03-05 09:04:02 +0200

    4. Testing theme

---
+++ dashboard/src/app/app-material.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+
+const modules = [
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatListModule
+];
+
+@NgModule({
+    imports: [
+        CommonModule,
+        ...modules
+    ],
+    exports: [
+        ...modules
+    ]
+})
+export class AppMaterialModule {}
