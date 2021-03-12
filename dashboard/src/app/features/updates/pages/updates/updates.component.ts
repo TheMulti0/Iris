3f2a4c99590556166088825eba2fef4f5d699393
TheMulti0 <multi@codeprecise.com>
2021-03-12 12:23:30 +0200

    Removed  scroll bar

--- dashboard/src/app/features/updates/pages/updates/updates.component.ts
+++ dashboard/src/app/features/updates/pages/updates/updates.component.ts
@@ -1,22 +1,15 @@
 import {
-  AfterViewInit,
-  ChangeDetectorRef,
   Component,
   ViewChild,
 } from '@angular/core';
 import {
   CdkVirtualScrollViewport,
-  ScrollDispatcher,
 } from '@angular/cdk/scrolling';
-import { Observable, BehaviorSubject, Subject, Subscription } from 'rxjs';
+import { Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Update } from 'src/app/models/updates';
 import { UpdatesService } from '../../services/updates.service';
-import {
-  DataSource,
-  CollectionViewer,
-  ListRange,
-} from '@angular/cdk/collections';
+import { PaginatedDataSource } from './PaginatedDataSource';
 
 enum Direction {
   Up = 'Up',
@@ -31,12 +24,6 @@ enum Direction {
 export class UpdatesComponent {
   @ViewChild(CdkVirtualScrollViewport)
   virtualScroll!: CdkVirtualScrollViewport;
-  updates$: Subject<Update[]> = new Subject<Update[]>();
-
-  pageSize = 12;
-  pageIndex = 0;
-
-  lastRangeEnd = 0;
 
   dataSource: PaginatedDataSource<Update>;
 
@@ -46,65 +33,8 @@ export class UpdatesComponent {
     );
   }
   trackByUrl(index: number, update: Update) {
-    return update;
+    return update.url;
   }
 }
 
-export class PaginatedDataSource<T> extends DataSource<T> {
-  private cachedItems: T[] = [];
-  private dataStream: Subject<T[]> = new BehaviorSubject<T[]>(this.cachedItems);
-  private subscription = new Subscription();
 
-  private pageSize = 10;
-  private currentPageIndex = 0;
-
-  constructor(
-    private getBatch: (pageIndex: number, pageSize: number) => Observable<T[]>
-  ) {
-    super();
-
-    this.fetchPage();
-  }
-
-  connect(
-    collectionViewer: CollectionViewer
-  ): Observable<T[] | ReadonlyArray<T>> {
-    const scrollSubscription = collectionViewer.viewChange.subscribe((range) =>
-      this.onNewRange(range)
-    );
-
-    this.subscription.add(scrollSubscription);
-
-    return this.dataStream;
-  }
-
-  onNewRange(range: ListRange) {
-    const pageIndex = this.getPageIndex(range.end, this.pageSize);
-
-    if (pageIndex === this.currentPageIndex) {
-      return;
-    }
-
-    this.currentPageIndex = pageIndex;
-    this.fetchPage();
-  }
-
-  disconnect(collectionViewer: CollectionViewer): void {
-    this.subscription.unsubscribe();
-  }
-
-  private async fetchPage() {
-    const batch: T[] = await this.getBatch(
-      this.currentPageIndex,
-      this.pageSize
-    ).toPromise();
-
-    this.cachedItems = this.cachedItems.concat(batch);
-
-    this.dataStream.next(this.cachedItems);
-  }
-
-  private getPageIndex(offset: number, pageSize: number): number {
-    return Math.max(0, Math.floor((offset + pageSize - 1) / pageSize));
-  }
-}
