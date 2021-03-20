19a18bd0e1502fd88a6efe7c05bbe6697d5a81c6
TheMulti0 <multi@codeprecise.com>
2021-03-20 22:36:37 +0200

    Format links

--- dashboardapp/src/app/shared/pipes/format.pipe.ts
+++ dashboardapp/src/app/shared/pipes/format.pipe.ts
@@ -1,10 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+const linkRegex = /(https:\/\/(\w|\.|\/)+)/;
+
 @Pipe({
   name: 'format',
 })
 export class FormatPipe implements PipeTransform {
   transform(value: string, ...args: any[]): string {
-    return value.split('\n').join('<br>');
+    return value
+      .split('\n').join('<br>')
+      .replace(linkRegex, '<a href="$1">$1</a>');
   }
 }
