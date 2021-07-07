0186cab6cbcbf1f836bbb7fc220c049d54ed664f
TheMulti0 <multi@codeprecise.com>
2021-07-07 08:29:46 +0300

    Increased device scale

--- MessagesManager/Screenshot/TwitterScreenshotter.cs
+++ MessagesManager/Screenshot/TwitterScreenshotter.cs
@@ -7,7 +7,7 @@ namespace MessagesManager
     internal class TwitterScreenshotter : IWebsiteScreenshotter
     {
         private const string TweetHtml = "<blockquote class=\"twitter-tweet\" style=\"width: 300px;\" data-dnt=\"true\">\r\n<p lang=\"en\" dir=\"ltr\"></p>\r\n\r\n<a href=\"{TWEET_URL}\"></a>\r\n\r\n</blockquote> <script async src=\"https://platform.twitter.com/widgets.js\" charset=\"utf-8\"></script>";
-        private const double DeviceScale = 2.5;
+        private const double DeviceScale = 3;
         private const string CssSelector = ".twitter-tweet";
         private static readonly TimeSpan Delay = TimeSpan.FromMilliseconds(1500);
         
