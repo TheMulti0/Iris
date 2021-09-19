790ef494b6bb8c9544b30e0ab5d18af5d2868008
TheMulti0 <multi@codeprecise.com>
2021-09-19 21:02:47 +0300

    Added Dashboard backend

---
+++ Dashboard/Controllers/PostsListenerSubscriptionsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PostsListener.Client;
+using Scraper.MassTransit.Common;
+
+namespace Dashboard.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PostsListenerSubscriptionsController : ControllerBase
+    {
+        private readonly INewPostSubscriptionsClient _client;
+
+        public PostsListenerSubscriptionsController(INewPostSubscriptionsClient client)
+        {
+            _client = client;
+        }
+
+        [HttpGet]
+        public Task<IEnumerable<Subscription>> Get(CancellationToken ct)
+        {
+            return _client.GetSubscriptionsAsync(ct);
+        }
+
+        [HttpPost("{platform}/{id}")]
+        public async Task AddOrUpdate(string id, string platform, TimeSpan pollInterval, DateTime earliestPost, CancellationToken ct)
+        {
+            await _client.AddOrUpdateSubscription(id, platform, pollInterval, earliestPost, ct);
+        }
+        
+        [HttpDelete("{platform}/{id}")]
+        public async Task Remove(string id, string platform, CancellationToken ct)
+        {
+            await _client.RemoveSubscription(id, platform, ct);
+        }
+    }
+}
---
+++ Dashboard/Controllers/TelegramSubscriptionsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SubscriptionsDb;
+
+namespace Dashboard.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TelegramSubscriptionsController : ControllerBase
+    {
+        private readonly IChatSubscriptionsRepository _repository;
+
+        public TelegramSubscriptionsController(IChatSubscriptionsRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IQueryable<SubscriptionEntity> Get()
+        {
+            return _repository.Get();
+        }
+    }
+}
