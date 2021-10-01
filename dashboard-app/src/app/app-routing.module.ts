b6043a239fd8f8799728681f64f89ce9bf70b0f2
TheMulti0 <multi@codeprecise.com>
2021-10-01 11:59:32 +0300

    Routes

--- dashboard-app/src/app/app-routing.module.ts
+++ dashboard-app/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostsListenerComponent } from './components/posts-listener/posts-listener.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'posts-listener',
+    component: PostsListenerComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/posts-listener',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
