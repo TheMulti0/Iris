d26b2a7b09ae1fb26255ec2d2e649a7689f23320
TheMulti0 <multi@codeprecise.com>
2021-10-13 20:11:02 +0300

    Format null captions

--- TelegramClient/Implementation/TdExtensions.cs
+++ TelegramClient/Implementation/TdExtensions.cs
@@ -22,13 +22,13 @@ namespace TelegramClient
             switch (content)
             {
                 case TdApi.InputMessageContent.InputMessageVideo v:
-                    return $"Video {v.Video.Format()}, Thumbnail {v.Thumbnail?.Thumbnail.Format()}, Caption length {v.Caption.Text.Length}, {v.Width}x{v.Height} size";
+                    return $"Video {v.Video.Format()}, Thumbnail {v.Thumbnail?.Thumbnail?.Format()}, Caption length {v.Caption?.Text?.Length}, {v.Width}x{v.Height} size";
                 
                 case TdApi.InputMessageContent.InputMessageText t:
-                    return $"Text {t.Text}, Caption length {t.Text.Text.Length}";
+                    return $"Text {t.Text}, Caption length {t.Text?.Text?.Length}";
                 
                 case TdApi.InputMessageContent.InputMessagePhoto p:
-                    return $"Photo {p.Photo.Format()}, Thumbnail {p.Thumbnail?.Thumbnail.Format()}, Caption length {p.Caption.Text.Length}, {p.Width}x{p.Height} size";
+                    return $"Photo {p.Photo.Format()}, Thumbnail {p.Thumbnail?.Thumbnail?.Format()}, Caption length {p.Caption?.Text?.Length}, {p.Width}x{p.Height} size";
                 
                 default:
                     return content.GetType().FullName;
